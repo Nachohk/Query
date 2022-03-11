184561b04513352bde6085455cdfdd6905a972d5
Nachohk <ignacio.mlucero@gmail.com>
2022-03-11 00:21:57 -0300

    Four Up

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+    
+$(document).ready(function(){
+    $("#qw").hide();
+    $ ("#misu").hide();
+    $ ("#teada").hide();
+
+    $("#pan").click(function(){
+        $ ("#qw").toggle(1000);
+    });
+
+
+    $("#tira").click(function(){
+        $ ("#misu").toggle(1000);
+    });   
+     
+
+    $("#plat").click(function(){
+        $ ("#teada").toggle(1000);
+    });   
+    
+    $("#enviar").click(function(){
+        alert("El correo fue enviado correctamente");
+     
+    });
+
+    $("#qwe2").dblclick(function(){
+        $(this).css("color", "red")
+    
+    });
+
+    $("#qwe").dblclick(function(){
+            $(this).css("color", "red")
+    
+    });
+
+});
